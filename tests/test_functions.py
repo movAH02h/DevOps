80b4a691369f6022a1f7e5041ffebf27174b1276
Kirill Rumyantsev <KARum2004@yandex.ru>
2024-04-11 09:46:03 +0300

    commit with five tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,2 +1,19 @@
+from binary_search import search
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 10, -1, -100, -99, 200, 33]
+targets = [1000, 3, 5, 11, -100]
+
 def test_first():
-    assert 1 == 1
+    assert search(array, 0, len(array) - 1, targets[0]) == targets[0]
+
+def test_second():
+    assert search(array, 0, len(array) - 1, targets[1]) == targets[1]
+
+def test_third():
+    assert search(array, 0, len(array) - 1, targets[2]) == targets[2]
+
+def test_fourth():
+    assert search(array, 0, len(array) - 1, targets[3]) == targets[3]
+
+def test_fifth():
+    assert search(array, 0, len(array) - 1, targets[4]) == targets[4]
